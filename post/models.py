dfebe06e60d8fa7e65c60fe3fd7ad378dcceed95
Jordan Haagenson <jdhaagenson@gmail.com>
2020-10-06 07:25:54 -0700

    post model beginnings

--- post/models.py
+++ post/models.py
@@ -1,3 +1,15 @@
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+
+def user_path(instance, filename):
+    return f'user_{instance.instauser.id}/{filename}'
+
+
+class Post(models.Model):
+    photo = models.ImageField(upload_to=user_path, width_field=models.IntegerField, height_field=models.IntegerField, max_length=100)
+    caption = models.CharField(max_length=250)
+    date = models.DateTimeField(default=timezone.now)
+    location = models.CharField(max_length=100, null=True, blank=True)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
