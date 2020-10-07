3450a4fbadfde67a3fe7d94ddf44d708a4137a58
Jordan Haagenson <jdhaagenson@gmail.com>
2020-10-07 13:00:54 -0700

    found correct views before the merge

--- post/views.py
+++ post/views.py
@@ -1,15 +1,21 @@
-from django.shortcuts import render, HttpResponseRedirect, reverse
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect, reverse
 from .models import Post
 from .forms import PostForm
 from django.views import View
-from django.views.generic import FeedView, TemplateView, ListView, DeleteView
+from django.views.generic import DetailView, CreateView, FormView, TemplateView, ListView, DeleteView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
 
-class FeedView(FeedView):
+class PostFeedView(View):
     model = Post
 
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
-class PostFormView(View):
+
+class PostFormView(CreateView):
     form_class = PostForm
     template_name = 'form.html'
 
@@ -27,4 +33,19 @@ class PostFormView(View):
                 instauser=request.user,
                 location=data.get('location')
             )
+            return HttpResponseRedirect('/feed/')
+        return render(request, self.template_name, {'form': form})
 
+
+def PostsByUserView(request, userid):
+    posts = Post.objects.filter(instauser_id=userid)
+    return render(request, 'profile.html', {'posts': posts})
+
+
+def PostDetailView(request, postid):
+    post = Post.objects.get(pk=postid)
+    return render(request, 'detail.html', {'post': post})
+
+
+def PostsByFollowers(request):
+    posts = Post.objects.filter(instauser_id=request.user.following)
