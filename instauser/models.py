a5f5c5335e3f276170333d553e67f02057b48fde
Jordan Haagenson <jdhaagenson@gmail.com>
2020-10-13 07:52:03 -0700

    needs default on model

--- instauser/models.py
+++ instauser/models.py
@@ -6,7 +6,7 @@ class InstaUser(AbstractUser):
     display_name = models.CharField(max_length=40, unique=False)
     bio = models.CharField(max_length=100, blank=True, null=True)
     followers = models.ManyToManyField("self", symmetrical=False, default='self')
-    profile_pic = models.ImageField(upload_to='image')
+    profile_pic = models.ImageField(upload_to='image', default='/media/image/default.jpg')
     REQUIRED_FIELDS = ['display_name']
 
     def __str__(self):
