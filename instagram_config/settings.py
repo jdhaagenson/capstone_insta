fcb6648eb7ea541e151f8d0525ad05d8d6cf84da
Jordan Haagenson <jdhaagenson@gmail.com>
2020-12-01 15:13:01 -0800

    got rid of some extra files

--- instagram_config/settings.py
+++ instagram_config/settings.py
@@ -135,7 +135,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
@@ -159,3 +159,5 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'django.contrib.auth.backends.AllowAllUsersModelBackend'
 ]
+
+# django_heroku.settings(locals())
